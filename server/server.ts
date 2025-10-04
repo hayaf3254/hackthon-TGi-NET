a79462df648d98f3b087c069fa92a12dca74d48d
hayaf3254 <poso42731427@gmail.com>
2025-10-05 08:04:04 +0900

    バックエンド:テストでcircleのCRDを作成、個別ユーザ情報取得を作成

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import fs from "node:fs";
 import express from "express";
 import cors from "cors";
 import testRouter from './test/test';
+import circleTestRouter from './test/circle';
+import userRouter from './user/user';
 
 //const express = require('express');
 const app = express();
@@ -19,5 +21,8 @@ console.log("Hello TypeScript on Windows!");
 console.log("files:", fs.readdirSync("."));
 
 app.use('/test', testRouter);
+app.use('/test/api', circleTestRouter);
+app.use('/api/user', userRouter);
+
 
 
